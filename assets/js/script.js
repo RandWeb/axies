74645fb5149aa8d81fb6371d8b43d52ec7520f1b
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-03-11 11:54:03 -0800

    complate box search

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,18 @@
  * header
  */
 
-
+ document.addEventListener('mouseup', function (e) {
+    var container = document.querySelector('.header__search-box');
+    if (!container.contains(e.target)) {
+        container.style.display = 'none';
+        console.log('invisible');
+    }
+});
 let iconSearch = document.querySelector('.search__icon')
-iconSearch.addEventListener('click', (t) => {
-    console.log('click')
+iconSearch.addEventListener('click', () => {
+    console.log('visible');
     document.getElementsByClassName('header__search-box')[0].style.display = "flex";
-  });
-  
-  iconSearch.addEventListener('blur', () => {
-      console.log('blur')
-    document.getElementsByClassName('header__search-box')[0].style.display = "none";
-  });
-
+})
 /**
  * popular pictures
  */
@@ -22,12 +22,12 @@ const pictures = document.querySelectorAll('.popular__pictures-item')
 
 function removeActiveClasses(obj) {
     console.log(obj)
- for (let index = 0; index < obj.length; index++) {
-     const node = obj[index];
-     if(node.classList.contains('popular__item-active')){
-        node.classList.remove('popular__item-active')
-     }
- }
+    for (let index = 0; index < obj.length; index++) {
+        const node = obj[index];
+        if (node.classList.contains('popular__item-active')) {
+            node.classList.remove('popular__item-active')
+        }
+    }
 }
 
 pictures.forEach((picture) => {
