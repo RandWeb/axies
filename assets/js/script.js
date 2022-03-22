ef70c9d99f380433606544e2c3439208b56232e5
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-03-22 01:33:10 -0700

    delete  popular  section

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,27 +73,3 @@ document.addEventListener('mouseup', function (e) {
         removeClassActiveSubmenu();
     }
 });
-
-/**
- * popular pictures
- */
-
-const pictures = document.querySelectorAll('.popular__pictures-item')
-
-function removeActiveClasses(obj) {
-    console.log(obj)
-    for (let index = 0; index < obj.length; index++) {
-        const node = obj[index];
-        if (node.classList.contains('popular__item-active')) {
-            node.classList.remove('popular__item-active')
-        }
-    }
-}
-
-pictures.forEach((picture) => {
-    picture.addEventListener('click', () => {
-        removeActiveClasses(picture.parentNode.children);
-        picture.classList.add('popular__item-active')
-    })
-})
-
