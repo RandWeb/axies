71b4247cc5eab580e6d59ef22ca2060916ce922a
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-04-02 08:18:24 -0700

    complate details page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,22 @@
 const lihteMode = document.querySelector('.header__sun');
 const darkMode = document.querySelector('.header__moon');
 let mode = document.querySelector('.header__mode h5');
-lihteMode.addEventListener('click',()=>{
-     let root = document.querySelector('html');
-     if(!root.classList.contains('theme-light')){
+lihteMode.addEventListener('click', () => {
+    let root = document.querySelector('html');
+    if (!root.classList.contains('theme-light')) {
         root.classList.remove('theme-dark');
         root.classList.add('theme-light');
-        mode.innerHTML="light mode<br/><b>available</b>"
-     }
+        mode.innerHTML = "light mode<br/><b>available</b>"
+    }
 })
-darkMode.addEventListener('click',()=>{
-     let root = document.querySelector('html');
+darkMode.addEventListener('click', () => {
+    let root = document.querySelector('html');
     let mode = document.querySelector('.header__mode h5');
-        if(!root.classList.contains('theme-dark')){
-            root.classList.remove('theme-light');
-            root.classList.add('theme-dark')
-            mode.innerHTML="dark mode<br/><b>available</b>"
-        }
+    if (!root.classList.contains('theme-dark')) {
+        root.classList.remove('theme-light');
+        root.classList.add('theme-dark')
+        mode.innerHTML = "dark mode<br/><b>available</b>"
+    }
 
 })
 
@@ -52,18 +52,22 @@ document.querySelector('.menu').addEventListener('click', () => {
 
 
 let navLinks = document.querySelectorAll('.navbar__link')
-navLinks.forEach((item)=>{
-   item.addEventListener('click', (e) => {
-        let submenu=item.querySelector('.sub__menu-one');
-         removeClassActiveSubmenu();
-        submenu.classList.add('active-submenu')
+navLinks.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        let submenu = item.querySelector('.sub__menu-one');
+        removeClassActiveSubmenu();
+        submenu.classList.add('active-submenu');
+        item.querySelector('i').classList.add('rotate');
         console.log(submenu);
     });
 });
 
-function removeClassActiveSubmenu(){
-    document.querySelectorAll('.sub__menu-one').forEach(item=>{
+function removeClassActiveSubmenu() {
+    document.querySelectorAll('.sub__menu-one').forEach(item => {
         item.classList.remove('active-submenu');
+        document.querySelectorAll('.navbar__link i').forEach(icon => {
+            icon.classList.remove('rotate')
+        });
     })
 }
 
@@ -80,17 +84,44 @@ document.addEventListener('mouseup', function (e) {
 let shareBtn = document.querySelector(".button");
 
 shareBtn.addEventListener("click", () => {
-  shareBtn.classList.toggle('show');
-  if (document.querySelector(".social.twitter").classList.contains("clicked")) {
-    document.querySelector(".social.twitter").classList.toggle("unclicked");
-    document.querySelector(".social.facebook").classList.toggle("unclicked");
-    document.querySelector(".social.discord").classList.toggle("unclicked");
-    document.querySelector(".social.youtube").classList.toggle("unclicked");
-  } else {
-    document.querySelector(".social.twitter").classList.toggle("clicked");
-    document.querySelector(".social.facebook").classList.toggle("clicked");
-    document.querySelector(".social.discord").classList.toggle("clicked");
-    document.querySelector(".social.youtube").classList.toggle("clicked");
-  }
+    shareBtn.classList.toggle('show');
+    if (document.querySelector(".social.twitter").classList.contains("clicked")) {
+        document.querySelector(".social.twitter").classList.toggle("unclicked");
+        document.querySelector(".social.facebook").classList.toggle("unclicked");
+        document.querySelector(".social.discord").classList.toggle("unclicked");
+        document.querySelector(".social.youtube").classList.toggle("unclicked");
+    } else {
+        document.querySelector(".social.twitter").classList.toggle("clicked");
+        document.querySelector(".social.facebook").classList.toggle("clicked");
+        document.querySelector(".social.discord").classList.toggle("clicked");
+        document.querySelector(".social.youtube").classList.toggle("clicked");
+    }
 });
+
+// details tabs
+
+const tabs = document.querySelectorAll('.extra__title h4');
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        console.log(`.extra__tabs #${tab.id}`)
+        RemoveShowTabClass();
+        RemoveActiveTabClass();
+        tab.classList.add('active')
+        let subtab = document.querySelector(`.extra__tabs #${tab.id}`);
+        subtab.classList.add('show-tab')
+        console.log(subtab)
+    })
+})
+
+function RemoveShowTabClass() {
+    document.querySelectorAll('.extra__tabs > li').forEach(tab => {
+        tab.classList.remove('show-tab')
+    })
+}
+function RemoveActiveTabClass() {
+    document.querySelectorAll('.extra__title h4').forEach(tab => {
+        tab.classList.remove('active')
+    })
+}
 
